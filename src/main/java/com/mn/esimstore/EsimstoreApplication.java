f03b72ba9db99640fc2bfb8e5204ac08eaade936
pudu <purevdulam.dash@gmail.com>
2024-10-09 15:47:59 +0800

    add

---
+++ src/main/java/com/mn/esimstore/EsimstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mn.esimstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsimstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsimstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mn/esimstore/EsimstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mn.esimstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsimstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
